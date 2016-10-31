b5932c41233e8aed102b27492a458a0c9753c21c
skylake\synoise <piotr.synos@gmail.com>
2016-10-31 17:24:46 +0100

    add  improvments

--- app/home/home_ctrl.js
+++ app/home/home_ctrl.js
@@ -18,11 +18,12 @@
 
 
     angular.module("iApp", ['smart-table', 'ngDraggable'])
-        .factory('myhttpserv', function ($http) {
+       /* .factory('myhttpserv', function ($http) {
+
             return $http.get('storage.txt').error(function (status) {
                 console.log(status)
             });
-        })
+        })*/
         .filter('unique', function () {
 
             return function (arr, field) {
@@ -41,21 +42,14 @@
                 if (collection === null) return;
                 return uniqueItems(collection, key);
             };
-        }).controller('TestController', function ($scope, $http, $filter, orderByFilter, filterFilter, myhttpserv) {
+        }).controller('TestController', function ($scope, $http, $filter, orderByFilter, filterFilter) {
 
 
-        myhttpserv.then(function (response) {
+      //  myhttpserv.then(function (response) {});, myhttpserv
 
-            //  $scope.todos = (response.data !== null) ? response.data : [];
-
-            /*   var httpPost = function () {
-             $http.post('save.php', {"aa":JSON.stringify($scope.todos)})
-             .error(function (status) {
-             console.log(status)
-             });
-             }; */
-
-            // $scope.todos = [{"aaa":"ccc"},{"aaa":"ccc"},{"aaa":"ccc"}]
+            $scope.addTodo1 = function (scope, file) {
+                alert(12)
+            }
 
             $scope.addTodo = function (scope, file) {
 
@@ -77,50 +71,7 @@
                 }
             };
 
-            /*$scope.remaining = function () {
-             var count = 0;
-             angular.forEach($scope.todos, function (todo) {
-             count += todo.doneProd && todo.doneDev ? 0 : 1;
-             });
-             return count;
-             };*
 
-             /* $scope.archive = function () {
-             var rusure = confirm("Are you sure you want to remove the completed tasks from the list?");
-             if (rusure) {
-             var oldTodos = $scope.todos;
-             $scope.todos = [];
-
-             angular.forEach(oldTodos, function (todo) {
-             if (!todo.doneProd || !todo.doneDev)
-             $scope.todos.push(todo);
-             });
-             httpPost();
-             }
-             };    */
-
-            /* $scope.delete = function (idx) {
-             var rusure = confirm("Are you sure you want to remove the task from the list?");
-             if (rusure) {
-             $scope.todos.splice(idx, 1);
-             httpPost();
-             }
-             };  */
-
-            /* $scope.edit = function (idx) {
-             var changes = prompt("Please make the changes below", $scope.todos[idx].text);
-             if (changes != null) {
-             $scope.todos[idx].text = changes;
-             httpPost();
-             }
-             };
-
-             $scope.checkboxClick = function () {
-             httpPost();
-             };
-
-             $('.splash, .container').fadeToggle();*/
-        });
 
 
         $scope.search = [];
@@ -205,7 +156,7 @@
                 $scope.pakiets = "";
                 $scope.reloadPakiety(pack);
 
-                $http({method: "GET", url: "./json/pakiety2.json?=" + pack+"?="+Math.random()}).then(function mySucces(response) {
+                $http({method: "GET", url: "./json/pakiety.json?" + pack}).then(function mySucces(response) {
 
                     $scope.packs = response.data[pack];
                     $http(
@@ -299,7 +250,7 @@
 
         $scope.reloadPakiety = function reloadPakiety(pack) {
             $scope.pakiety5admin = 'block';
-            $scope.loadJSONtoScope2("./json/pakiety2.json", pack, $scope.pakiety);
+            $scope.loadJSONtoScope2("./json/pakiety.json", pack, $scope.pakiety);
             $scope.pakiets = "./subsites/pakiety5.html";
         };
 
